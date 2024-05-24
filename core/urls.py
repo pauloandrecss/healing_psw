dcb0cb41764daa26855bceac167c2c80aed56d8a
Paulo André Cardoso Silva Santos <pauloandrecss@gmail.com>
2024-05-24 02:03:37 -0300

    root route configured

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/usuarios/cadastro')),
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
     path('medicos/', include('medico.urls')),
